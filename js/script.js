ca21bb3cd7a8bd3177351ed0a6d84c2e5d712612
Olga <olg200919@ya.ru>
2022-11-26 16:22:03 +0300

    Задача2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Задача 1
+/* Задача 1
 num1 =  Math.pow(2, 3);
 num2 =  Math.pow(3, 3);
-console.log(num1 + num2);
+console.log(num1 + num2);*/
 
+/*Задача 2   */
+
+let maney = Number(prompt('Введите размер заработной платы'));
+if (isNaN(maney)) {
+    alert('Значение задано неверно!');
+}
+const wages = (maney) => {
+    maney = maney * 0.87;
+    console.log(maney);
+}
+wages(maney);
