cd6fee1cd0057d3523ba54010333cebf19291139
Olga <olg200919@ya.ru>
2022-11-26 16:37:32 +0300

    Задача4

--- js/script.js
+++ js/script.js
@@ -14,9 +14,41 @@ const wages = (maney) => {
 }
 wages(maney);*/
 
-/*Задача 3   */
+/*Задача 3  
 let a = Number(prompt('Введите первое число'));
 let b = Number(prompt('Введите второе число'));
 let c = Number(prompt('Введите третье число'));
 const max = Math.max(a, b, c);
-console.log(`${max} - наибольшее число`);
+console.log(`${max} - наибольшее число`);*/
+
+/*Задача 4 */
+
+let num1 = Number(prompt('Введите первое число'));
+let num2 = Number(prompt('Введите второе число'));
+
+function summNumber(num1, num2) {
+    summ = num1 + num2;
+    console.log(summ);
+}
+
+function diffNumber(num1, num2) {
+    max = Math.max(num1, num2);
+    min = Math.min(num1, num2);
+    diff = max - min;
+    console.log(diff);
+}
+
+function multiplNumber(num1, num2) {
+    multipl = num1 * num2;
+    console.log(multipl);
+}
+
+function divNumber(num1, num2) {
+    div = num1 / num2;
+    console.log(div);
+}
+
+summNumber(num1, num2);
+diffNumber(num1, num2);
+multiplNumber(num1, num2);
+divNumber(num1, num2);
